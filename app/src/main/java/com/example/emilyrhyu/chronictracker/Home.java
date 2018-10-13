2c65d6ffb1e72b76a388e2cbc2135adb081fe348
Alex Pereira <alex.pereira@outlook.com>
2018-10-13 16:55:05 -0400

    POPUP!

--- app/src/main/java/com/example/emilyrhyu/chronictracker/Home.java
+++ app/src/main/java/com/example/emilyrhyu/chronictracker/Home.java
@@ -1,26 +1,61 @@
 package com.example.emilyrhyu.chronictracker;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
 
 public class Home extends Fragment {
 
-    // The onCreateView method is called when Fragment should create its View object hierarchy,
-    // either dynamically or via XML layout inflation.
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
-        // Defines the xml file for the fragment
-        return inflater.inflate(R.layout.fragment_home, parent, false);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
     }
 
-    // This event is triggered soon after onCreateView().
-    // Any view setup should occur here.  E.g., view lookups and attaching view listeners.
+    View view;
+
     @Override
-    public void onViewCreated(View view, Bundle savedInstanceState) {
-        // Setup any handles to view objects here
-        // EditText etFoo = (EditText) view.findViewById(R.id.etFoo);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+        Button buttonClick = rootView.findViewById(R.id.yo);
+
+
+        buttonClick.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                // if play name already exists
+                AlertDialog.Builder dialog = new AlertDialog.Builder(getActivity());
+                dialog.setCancelable(false);
+                dialog.setMessage("Player Name Already Exists. Please Find a New Name.");
+                dialog.setPositiveButton("Okay", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+                dialog.show();
+
+            }
+        });
+
+        return rootView;
     }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
 }
